18ac60616df3e0eda2aac14ee746f6ca5701d869
Habibata Samake <habibata.samake@grenoble-inp.org>
2025-01-14 20:57:33 +0100

    Add fichier.sh

---
+++ sup_fichier_av_set_data.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+rm -vfR Ensimag-SlicesPneumothorax/Test/imgs/;
+rm -vfR Ensimag-SlicesPneumothorax/Test/masks/;
+mkdir Ensimag-SlicesPneumothorax/Test/masks/;
+mkdir Ensimag-SlicesPneumothorax/Test/imgs/;
+rm -vfR Ensimag-SlicesPneumothorax/Train_Validation/imgs/;
+rm -vfR Ensimag-SlicesPneumothorax/Train_Validation/masks/;
+mkdir Ensimag-SlicesPneumothorax/Train_Validation/imgs/;
+mkdir Ensimag-SlicesPneumothorax/Train_Validation/masks/;
